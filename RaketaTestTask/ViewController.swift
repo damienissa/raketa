73292faf888518e92b73d63db1a0cb81c29e45d2
Dmytro Virych <damienissa@yahoo.com>
2020-10-21 14:08:15 +0300

    Initial Commit

---
+++ RaketaTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RaketaTestTask
+//
+//  Created by Dima Virych on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
